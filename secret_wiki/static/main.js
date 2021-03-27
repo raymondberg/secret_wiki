8fc888967366c515465eb9a281d8386ea555aa9d
Raymond Berg <raymond.berg@getbraintree.com>
2021-03-27 01:28:08 +0000

    Add logout

--- secret_wiki/static/main.js
+++ secret_wiki/static/main.js
@@ -8,17 +8,37 @@ function jsonUnescape(str)  {
   return str.replace(/\\\\n/g, "\n").replace(/\\\\r/g, "\r").replace(/\\\\t/g, "\t");
 }
 
+function logout() {
+  $.ajax({
+     url: "/api/auth/logout",
+     action: "POST",
+  })
+  .always(function() { window.location.replace('/login') })
+}
+
 function checkLogin(){
   $.ajax({
-    url: "/api/users/me"
+     url: "/api/users/me"
   })
+   .done(function(result) {
+     var status = $("#status")
+     status.empty()
+
+     status.append(
+       $("<a>")
+         .addClass("header-link")
+         .attr("href", "#")
+         .click(logout)
+         .text("Logout")
+     )
+   })
    .fail(function(result) {
-    window.location.replace('/login')
+     window.location.replace('/login')
    })
 }
 
 $( document ).ready(function() {
-  checkLogin()
+  user = checkLogin()
   loadWikis()
 
   wikiId = $.urlParam('w');
