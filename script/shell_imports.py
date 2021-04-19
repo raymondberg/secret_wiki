615c2069aed195b8bcadc4361275549253949224
raymondberg <dev@raymondberg.com>
2021-04-18 23:45:46 -0500

    Add create user helper to shell

--- script/shell_imports.py
+++ script/shell_imports.py
@@ -1,7 +1,23 @@
+import asyncio
 starting_locals = set(locals())
 
+from secret_wiki.api.auth import fastapi_users
 from secret_wiki.db import get_db
 from secret_wiki.models import Wiki, Page, Section
+from secret_wiki.schemas import UserShellCreate
+
+def create_user(*args, **kwargs):
+    async def create_user_(email, password, is_active=False, is_superuser=False, is_verified=True):
+        return await fastapi_users.create_user(
+            UserShellCreate(
+                email=email,
+                password=password,
+                is_active=is_active,
+                is_superuser=is_superuser,
+                is_verified=is_verified,
+            )
+        )
+    return asyncio.run(create_user_(*args, **kwargs))
 
 db = next(get_db())
 
