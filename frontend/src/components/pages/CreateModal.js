a6f1daacb1a2d1569ddc2e7f35e414af1130df11
raymondberg <dev@raymondberg.com>
2022-04-13 23:25:11 -0500

    Convert page modal and get working (broken since reduxification)

--- frontend/src/components/pages/CreateModal.js
+++ frontend/src/components/pages/CreateModal.js
@@ -1,107 +1,103 @@
-import React from "react";
+import { React, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { SecretButton } from "../buttons/SecretButton";
+import { useSelector, useDispatch } from "react-redux";
+import { invalidatePagesCache, updatePageBySlug } from "../../shared/wikiSlice";
 
-class PageCreateModal extends React.Component {
-  constructor(props) {
-    super(props);
+export default function PageCreateModal(props) {
+  const [title, setTitle] = useState(null);
+  const [slug, setSlug] = useState(null);
+  const [isSecret, setIsSecret] = useState(false);
+  const activeWiki = useSelector((state) => state.wiki.wiki);
+  const dispatch = useDispatch();
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handlePageCreate = this.handlePageCreate.bind(this);
-    this.state = { title: null, slug: null, isAdminOnly: false };
-  }
-
-  handlePageCreate() {
+  function handlePageCreate() {
     var body = {
-      title: this.state.title,
-      is_admin_only: this.state.isAdminOnly,
+      title: title,
+      is_admin_only: isSecret,
     };
 
-    if (this.state.slug !== null) {
-      body.slug = this.state.slug;
+    if (slug !== null) {
+      body.slug = slug;
     }
 
-    this.props.api
-      .post(`w/${this.props.wikiSlug}/p`, body)
+    props.api
+      .post(`w/${activeWiki.slug}/p`, body)
       .then((response) => response.json())
       .then((page) => {
-        this.setState({ title: null, slug: null });
-        this.props.handlePageCreate(page.slug);
+        setTitle(null);
+        setSlug(null);
+        dispatch(updatePageBySlug(page.slug));
+        dispatch(invalidatePagesCache());
+        props.handleClose();
       });
   }
 
-  handleChange(event) {
+  function handleChange(event) {
     if (event.target.name === "page_title") {
-      this.setState({ title: event.target.value });
+      setTitle(event.target.value);
     } else if (event.target.name === "page_slug") {
-      this.setState({ slug: event.target.value });
+      setSlug(event.target.value);
     } else if (event.target.name === "isSecret") {
-      this.setState({ isAdminOnly: !this.state.isAdminOnly });
+      setIsSecret(!isSecret);
       event.target.blur();
     }
   }
 
-  render() {
-    return (
-      <Modal
-        show={this.props.shouldShow}
-        onHide={this.props.handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Create Page</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="row">
-            <div className="col-md-9 py-3">
-              <div className="row">
-                <div className="col-md-3 py-3">
-                  <b>Page Title:</b>
-                </div>
-                <div className="col-md-9 py-3">
-                  <input
-                    type="text"
-                    name="page_title"
-                    onChange={this.handleChange}
-                    value={this.state.title ? this.state.title : ""}
-                  />
-                </div>
-                <div className="col-md-3">
-                  <em>Slug: (optional)</em>
-                </div>
-                <div className="col-md-9">
-                  <input
-                    type="text"
-                    name="page_slug"
-                    onChange={this.handleChange}
-                    value={this.state.slug ? this.state.slug : ""}
-                  />
-                </div>
+  return (
+    <Modal
+      show={props.shouldShow}
+      onHide={props.handleClose}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Create Page</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="row">
+          <div className="col-md-9 py-3">
+            <div className="row">
+              <div className="col-md-3 py-3">
+                <b>Page Title:</b>
+              </div>
+              <div className="col-md-9 py-3">
+                <input
+                  type="text"
+                  name="page_title"
+                  onChange={handleChange}
+                  value={title ? title : ""}
+                />
+              </div>
+              <div className="col-md-3">
+                <em>Slug: (optional)</em>
+              </div>
+              <div className="col-md-9">
+                <input
+                  type="text"
+                  name="page_slug"
+                  onChange={handleChange}
+                  value={slug ? slug : ""}
+                />
               </div>
             </div>
-            <div className="col-md-3 py-3">
-              <ButtonGroup vertical size="lg">
-                <Button variant="primary" onClick={this.handlePageCreate}>
-                  Create
-                </Button>
-                <Button variant="secondary" onClick={this.props.handleClose}>
-                  Close
-                </Button>
-                <SecretButton
-                  isSecret={this.state.isAdminOnly}
-                  onChange={this.handleChange}
-                />
-              </ButtonGroup>
-            </div>
           </div>
-        </Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
-    );
-  }
+          <div className="col-md-3 py-3">
+            <ButtonGroup vertical size="lg">
+              <Button variant="primary" onClick={handlePageCreate}>
+                Create
+              </Button>
+              <Button variant="secondary" onClick={props.handleClose}>
+                Close
+              </Button>
+              <SecretButton isSecret={isSecret} onChange={handleChange} />
+            </ButtonGroup>
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
 }
-
-export default PageCreateModal;
