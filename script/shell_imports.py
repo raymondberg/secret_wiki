80edccdbd7216806442071e208cbd9f219773f13
raymondberg <dev@raymondberg.com>
2022-04-15 17:53:15 -0500

    Revise old shell script

--- script/shell_imports.py
+++ script/shell_imports.py
@@ -1,26 +1,36 @@
-import asyncio
+import getpass
 
 starting_locals = set(locals())
 
-from secret_wiki.api.auth import fastapi_users
-from secret_wiki.db import get_db
-from secret_wiki.models import Page, Section, Wiki
-from secret_wiki.schemas import UserShellCreate
+import asyncio
+import uuid
+
+from fastapi_users.password import PasswordHelper
+
+from secret_wiki.db import DB, User
+from secret_wiki.models.wiki import Page, Section, SectionPermission, Wiki
 
 
 def create_user(*args, **kwargs):
-    async def create_user_(email, password, is_active=False, is_superuser=False, is_verified=True):
-        return await fastapi_users.create_user(
-            UserShellCreate(
-                email=email,
-                password=password,
-                is_active=is_active,
-                is_superuser=is_superuser,
-                is_verified=is_verified,
+    async def create_user_(
+        email=None, password=None, is_active=True, is_superuser=False, is_verified=True
+    ):
+        async with DB() as session:
+            session.add(
+                User(
+                    id=uuid.uuid4(),
+                    email=email or input("Email: "),
+                    hashed_password=PasswordHelper().hash(
+                        password or getpass.getpass("Password: ")
+                    ),
+                    is_active=is_active,
+                    is_superuser=is_superuser,
+                    is_verified=is_verified,
+                )
             )
-        )
+            await session.commit()
+
     return asyncio.run(create_user_(*args, **kwargs))
 
-db = next(get_db())
 
 print(f"Preloaded: {', '.join(sorted(set(locals()) - starting_locals))}")
