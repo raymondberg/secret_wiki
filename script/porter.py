00aa596bd2cd76a810ca5ca0ede379c1fcc7d861
raymondberg <dev@raymondberg.com>
2022-04-15 22:59:55 -0500

    Fixes to the export function

--- script/porter.py
+++ script/porter.py
@@ -9,8 +9,8 @@ def error_out(msg):
     sys.exit(1)
 
 
-async def do_export():
-    with open("export.yaml", "w") as file_handle:
+async def do_export(location):
+    with open(location, "w") as file_handle:
         await Exporter(file_handle).run()
 
 
@@ -23,10 +23,13 @@ if __name__ == "__main__":
     try:
         target = sys.argv[-1]
         command = sys.argv[-2]
-    except IndexError:
+        assert command in ("import", "export")
+    except AssertionError:
         error_out("Must be one of import/export")
     except AssertionError:
         error_out("usage: [import|export] [filename]")
+
+    print(f"{command}ing to {target}")
 
     if command == "import":
         asyncio.run(do_import(target))
