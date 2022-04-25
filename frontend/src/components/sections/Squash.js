ddab430f9aff58f36752afe65700d05eb0b7e2b8
raymondberg <dev@raymondberg.com>
2022-04-24 19:56:39 -0500

    Colorize secrets in squash mode to differentiate them

--- frontend/src/components/sections/Squash.js
+++ frontend/src/components/sections/Squash.js
@@ -2,15 +2,20 @@ import marked from "marked";
 import DOMPurify from "dompurify";
 import { linkReplace } from "../../common.js";
 
+function clean(text) {
+  return marked(DOMPurify.sanitize(linkReplace(text)));
+}
 export default function SectionSquash(props) {
-  const convertedSections = props.sections
-    .map((s) => s.prior_content)
-    .join("\n\n");
+  const convertedSections = props.sections.map((s) =>
+    s.is_secret
+      ? `<div class="secret-text">${clean(s.prior_content)}\n</div>`
+      : clean(s.prior_content)
+  );
   return (
     <div className="section-squash">
       <div
         dangerouslySetInnerHTML={{
-          __html: marked(DOMPurify.sanitize(linkReplace(convertedSections))),
+          __html: convertedSections.join("\n\n"),
         }}
       />
     </div>
