2c9399da0cea1f4f1e7288cfabda63bb5be85a2c
raymondberg <dev@raymondberg.com>
2022-04-25 22:25:25 -0500

    Improve styling

--- frontend/src/components/pages/Link.js
+++ frontend/src/components/pages/Link.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getLock } from "../Icons";
 
 export function fromObject(pageLinkObject, gotoPage) {
@@ -11,14 +12,48 @@ export function fromObject(pageLinkObject, gotoPage) {
 }
 
 export function PageLink(props) {
+  const [showChildren, setShowChildren] = useState(false);
+
+  const prefix =
+    props.data.children.length > 0 ? (
+      showChildren ? (
+        <i className="gg-icon gg-chevron-down"></i>
+      ) : (
+        <i className="gg-icon gg-chevron-right"></i>
+      )
+    ) : (
+      <i className="gg-icon"></i>
+    );
   const lock = props.data.is_secret ? getLock() : "";
+
+  function children() {
+    if (showChildren && props.data.children.length > 0) {
+      return (
+        <div className="px-3">
+          {props.data.children.map((l) => fromObject(l, props.gotoPage))}
+        </div>
+      );
+    }
+    return "";
+  }
+
   return (
     <div className="tree-page font-bold">
-      {props.prefix}{" "}
-      <span onClick={(e) => props.gotoPage(props.data.slug)}>
-        {props.data.title} {lock}
-      </span>
-      {props.data.children.map((l) => fromObject(l, props.gotoPage))}
+      <div className="d-flex align-items-left px-0">
+        <span
+          className="d-inline-block"
+          onClick={(e) => setShowChildren(!showChildren)}
+        >
+          {prefix}
+        </span>{" "}
+        <span
+          className="d-inline-block"
+          onClick={(e) => props.gotoPage(props.data.slug)}
+        >
+          {props.data.title} {lock}
+        </span>
+      </div>
+      {children()}
     </div>
   );
 }
