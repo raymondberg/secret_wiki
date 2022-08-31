127c5750ff016330137b5cbfe5c5a8ca3dfbe32a
raymondberg <dev@raymondberg.com>
2022-08-30 19:42:26 -0500

    Change logout button

--- frontend/src/components/buttons/LogoutButton.js
+++ frontend/src/components/buttons/LogoutButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logout_icon from "../../images/logout.png";
 
 export default function LogoutButton(props) {
   return (
     <div id="status">
       <button className="btn btn-light" onClick={props.logout}>
-        &#127916;
+        <img src={logout_icon} className="logout gg-icon" alt="Logout" />
       </button>
     </div>
   );
