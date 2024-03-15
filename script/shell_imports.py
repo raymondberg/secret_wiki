ecc975e0883ba2eb96608287a08a615c9ba18853
raymondberg <dev@raymondberg.com>
2024-03-14 19:17:57 -0500

    Add script for creating wiki

--- script/shell_imports.py
+++ script/shell_imports.py
@@ -11,26 +11,35 @@ from secret_wiki.db import DB, User
 from secret_wiki.models.wiki import Page, Section, SectionPermission, Wiki
 
 
-def create_user(*args, **kwargs):
-    async def create_user_(
-        email=None, password=None, is_active=True, is_superuser=False, is_verified=True
-    ):
-        async with DB() as session:
-            session.add(
-                User(
-                    id=uuid.uuid4(),
-                    email=email or input("Email: "),
-                    hashed_password=PasswordHelper().hash(
-                        password or getpass.getpass("Password: ")
-                    ),
-                    is_active=is_active,
-                    is_superuser=is_superuser,
-                    is_verified=is_verified,
-                )
+async def create_user_(
+    email=None, password=None, is_active=True, is_superuser=False, is_verified=True
+):
+    async with DB() as session:
+        session.add(
+            User(
+                id=uuid.uuid4(),
+                email=email or input("Email: "),
+                hashed_password=PasswordHelper().hash(password or getpass.getpass("Password: ")),
+                is_active=is_active,
+                is_superuser=is_superuser,
+                is_verified=is_verified,
             )
-            await session.commit()
+        )
+        await session.commit()
 
+
+def create_user(*args, **kwargs):
     return asyncio.run(create_user_(*args, **kwargs))
+
+
+async def create_wiki_(slug=None, name=None):
+    async with DB() as session:
+        session.add(Wiki(id=uuid.uuid4(), slug=slug, name=name))
+        await session.commit()
+
+
+def create_wiki(*args, **kwargs):
+    return asyncio.run(create_wiki_(*args, **kwargs))
 
 
 print(f"Preloaded: {', '.join(sorted(set(locals()) - starting_locals))}")
